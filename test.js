f48fa09111076bfb10da6be83c69e8164f30f26c
Jaehyuk Lee <jhlumd@gmail.com>
2020-01-08 17:10:17 -0800

    test 8

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const test = function(seconds) {
+const test = function(seconds, unit) {
   const minutes = seconds / 60;
   const hours = minutes / 60;
   const days = hours / 24;
@@ -7,7 +7,14 @@ const test = function(seconds) {
   const decades = years / 10;
   const centuries = years / 100;
 
+  switch(unit) {
+    case "minutes":
+      break;
+    default:
+      return seconds;
+  }
+
 };
 
-console.log(test(1234));
+console.log(test(1234, "hours"));
 
